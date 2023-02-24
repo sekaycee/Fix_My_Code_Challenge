e2dc051cb456e8b6f7f375c8dc483a801c5e1191
Kay Cee <se.kaycee@gmail.com>
2023-02-24 09:56:57 +0100

    remove No pwd

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -98,6 +98,6 @@ password")
     if user_1.is_valid_password(89):
         print("is_valid_password should return False if compare with integer")
 
-    if user_2.is_valid_password("No pwd"):
+    if user_2.is_valid_password(""):
         print("is_valid_password should return False if no password set \
 before")
