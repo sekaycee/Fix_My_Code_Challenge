2d65e814a298e4425651c4d61aa4523f738cd3a7
Kay Cee <se.kaycee@gmail.com>
2023-04-19 05:58:11 +0100

    [Fix] refactor square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def permiter_of_my_square(self):
+    def perimeter_of_my_square(self):
         """ Perimeter of the Square """
         return (self.width * 2) + (self.height * 2)
 
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.perimeter_of_my_square())
